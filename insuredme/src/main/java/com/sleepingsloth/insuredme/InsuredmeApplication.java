3a9b4160993c2e2fee27200b7f9338c8488df430
theoand <theoand@gmail.com>
2018-01-26 23:48:46 +0200

    Added web service example

--- insuredme/src/main/java/com/sleepingsloth/insuredme/InsuredmeApplication.java
+++ insuredme/src/main/java/com/sleepingsloth/insuredme/InsuredmeApplication.java
@@ -1,8 +1,11 @@
-package com.sleepingsloth.insuredme;
+	package com.sleepingsloth.insuredme;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +16,15 @@ public class InsuredmeApplication {
 	String home() {
 		return "Hello World!";
 	}
+	
+	 private static final String template = "Hello, %s!";
+	    private final AtomicLong counter = new AtomicLong();
+
+	    @RequestMapping("/greeting")
+	    public User greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	        return new User(counter.incrementAndGet(),
+	                            String.format(template, name));
+	    }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(InsuredmeApplication.class, args);
